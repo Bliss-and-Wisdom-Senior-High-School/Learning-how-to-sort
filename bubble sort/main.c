5eca7ce6dd32190a187eccea94a37b9af72343b5
albertwang1104 <80242923+albertwang1104@users.noreply.github.com>
2021-05-31 14:01:27 +0800

    Add files via upload

---
+++ bubble sort/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b){
+	int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void bubble_sort(int *arr, int len){
+	int i=0,j=0;
+    for(i = 0; i < len; i++) {
+        for(j = 0; j < len-1-i; j++) {
+            if(arr[j+1] < arr[j]) {
+            	swap(&arr[j+1],&arr[j]);
+ 	     	}
+	   }
+	}
+}
