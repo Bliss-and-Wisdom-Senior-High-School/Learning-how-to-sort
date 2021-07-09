512c5edb8607940f39fc59f6aca074d7dcb70e9a
albertwang1104 <80242923+albertwang1104@users.noreply.github.com>
2021-07-09 09:10:32 +0800

    Add files via upload

--- test/main.c
+++ test/main.c
@@ -1,28 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main() {
 	int array[10] =  {1,5,9,3,6,8,2,4,7};
 	int i=0; 
 	
     printf("before sort\n");
-    	
-	for(i=0;i<10;i++){
-        printf("%d ", array[i]);
-    }
+	PrintArray(array, 10);
 	printf("\n");
+	
 	printf("\n");
     
-    selection_sort(array, 10);
+    QuickSort(array, 0, 9);
 
     printf("after sort\n");
+    PrintArray(array, 10);
     
-	for(i=0;i<10;i++){
-        printf("%d ", array[i]);
-    }
 	return 0;
+}
+
+void PrintArray(int arr[], int len){
+	int i;
+	for (i = 0; i < len; i++)
+		printf("%d ", arr[i]);
+}
+
+void QuickSort(int arr[], int front, int end) {
+	if (front < end) {
+		int pivot = Partition(arr, front, end);
+		QuickSort(arr, front, pivot-1);
+		QuickSort(arr, pivot+1, end);
+	}
+}
+
+int Partition(int arr[], int front, int end) {
+	int pivot = arr[end];
+	int i = front-1, j;
+	
+	for (j = front; j < end; j++) {
+		if (arr[j] < pivot) {
+			i++;
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	i++;
+	swap(&arr[i], &arr[end]);
+	return i;
+}
+
+void MergeSort(int arr[], int left, int right) {
+	if (left < right) {
+		int middle = (left+right)/2;
+		
+		MergeSort(arr, left, middle);
+		MergeSort(arr, middle+1, right);
+		Merge(arr, left, middle, right);
+	}
+}
+
+void Merge(int arr[], int l, int m, int r) {
+	int i, j, k;
+	int n1 = m-l+1, n2 = r-m;
+	int left[n1], right[n2];
+	
+	for (i = 0; i < n1; i++)
+		left[i] = arr[l+i];
+	for (j = 0; j < n2; j++)
+		right[j] = arr[m+1+j];
+
+	i = 0;
+	j = 0;
+	k = l;
+	while (i < n1 && j < n2) {
+		if(left[i] <= right[j]) {
+			arr[k] = left[i];
+			i++;	
+		}
+		else {
+			arr[k] = right[j];
+			j++;
+		}
+		k++;
+	}
+	
+	while (i < n1) {
+		arr[k] = left[i];
+		i++;
+		k++;
+	}
+	
+	while (j < n2) {
+		arr[k] = right[j];
+		j++;
+		k++;
+	}
 }
 
 void swap(int *a, int *b){
@@ -31,8 +102,8 @@ void swap(int *a, int *b){
     *b=temp;
 }
 
-void bubble_sort(int *arr, int len){
-	int i=0,j=0;
+void BubbleSort(int arr[], int len){
+	int i=0, j=0;
     for(i = 0; i < len; i++) {
         for(j = 0; j < len-1-i; j++) {
             if(arr[j+1] < arr[j]) {
@@ -42,8 +113,8 @@ void bubble_sort(int *arr, int len){
 	}
 }
 
-void selection_sort(int *arr, int len){
-	int i=0, j=0, min,k;
+void SelectionSort(int arr[], int len){
+	int i=0, j=0, min;
 	for(i=0;i<len-1;i++){
 		min=i;
 		for(j=i;j<len;j++){
@@ -57,12 +128,18 @@ void selection_sort(int *arr, int len){
 	}
 }
 
-
-
-
-
-
-
+void InsertionSort(int arr[], int len){
+	int i, preIndex, n;
+	for(i = 1; i < len; i++){
+		preIndex=i-1;
+		n=arr[i];
+		while(n < arr[preIndex] && preIndex >= 0){
+			arr[preIndex+1]=arr[preIndex];
+			preIndex-=1;
+		}
+		arr[preIndex+1]=n;
+	}
+}
 
 
 
